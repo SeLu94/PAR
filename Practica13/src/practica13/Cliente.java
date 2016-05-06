6012fbcc69ec27050c27224971274721ee363b3d
Jose Luis <metaldonna666@gmail.com>
2016-05-06 04:26:15 +0200

    Ahora si es el último commit. :D

--- Practica13/src/practica13/Cliente.java
+++ Practica13/src/practica13/Cliente.java
@@ -11,26 +11,31 @@ public class Cliente {
 
     private static String IP = "127.0.0.1";
     private static int Puerto = 2000;
-    /*Esta es la cadena que se usará como ping, cada caracter son 16 bits*/
-    private static String envio ="ping";
 
     public static void main(String args[]) throws IOException {
         ping();
     }
 
     private static int bitString(String str) {
-        /*La lectura de 1 caracter en Java con str.length() es 16 bits por caracter*/
+        /*
+        La lectura de 1 caracter en Java con str.length() es 16 bits por caracter,
+        como usamos la secuencia como la cadena a enviar, a partir del 10 ping, el
+        envío será de 32 bits.
+        */
         int bits = str.length() * 16;
         return bits;
     }
 
     private static void ping() throws IOException {
         long t1, t2;
+        int secuencia =0;
         while (true) {
+
+            String envio = ""+secuencia+"";
+            secuencia++;
             try {
                 /* Creamos una conexión a una ip y un puerto*/
                 Socket soc = new Socket(IP, Puerto);
-
                 System.out.println("Haciendo ping a: " + IP + ":" + Puerto + " con " + bitString(envio) + " bits");
                 /*Creamos un flujo de salida, en el cual mandaremos la cadena de caracteres "envio" */
                 OutputStream os = soc.getOutputStream();
@@ -41,7 +46,7 @@ public class Cliente {
                 /*Recogemos la hora en milisegundos, antes del envío*/
                 t1 = System.currentTimeMillis();
                 /*Enviamos la cadena*/
-                pw.println(envio);
+                pw.println(secuencia);
                 /*
                 Como usamos flujos, hasta que no se ejecute esta linea de lectura,
                 la cual será la respuesta del servidor, esta no pasará a ejecutar la siguiente
@@ -49,7 +54,7 @@ public class Cliente {
                 String respuesta = br1.readLine();
                 /*Recogemos la hora en milisegundos, tras la respuesta*/
                 t2 = System.currentTimeMillis();
-                System.out.println("Tiempo de respuesta aproximado: " + (t2 - t1) + " milisegundos. Pérdida: " + (bitString(envio) - bitString(respuesta)) + " bits.");
+                System.out.println(respuesta+": Tiempo de respuesta aproximado: " + (t2 - t1) + " milisegundos. Pérdida: " + (bitString(envio) - bitString(respuesta)) + " bits.");
                 /*Hacemos una espera para que el ping sea legible*/
                 Thread.sleep(1000);
             } catch (Exception e) {
